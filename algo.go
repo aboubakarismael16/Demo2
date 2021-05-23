d7be43d770097edf3d03f4f635138d7a873de564
aboubakar ismael <aboubakarismael16@gmail.com>
2021-05-23 20:02:16 +0800

    second commit

--- algo.go
+++ algo.go
@@ -9,67 +9,66 @@ func takeCupCake(CupeCake []int) int {
 	return CupeCake[0]
 }
 
-func eatChips(blowOfChips int)  {
+func eatChips(blowOfChips int) {
 
-	if chip := 0 ; chip < blowOfChips {
+	if chip := 0; chip < blowOfChips {
 		fmt.Println("Have some chips")
 		return
-	}else {
+	} else {
 		fmt.Println("No more chips")
 	}
 
 }
 
-func pizzaDelivery(boxeDelivered int)  {
+func pizzaDelivery(boxeDelivered int) {
 
-	for pizzaBox := 0 ; pizzaBox < boxeDelivered; pizzaBox++{
+	for pizzaBox := 0; pizzaBox < boxeDelivered; pizzaBox++ {
 		//for pizza := 0 ; pizza <= pizzaBox; pizza ++{
 		//	for slice := 0; slice <= pizza ; slice++ {
-				fmt.Println("Pizza is Here," +
-					"there are  boxes delivered: ",boxeDelivered)
-				return
+		fmt.Println("Pizza is Here,"+
+			"there are  boxes delivered: ", boxeDelivered)
+		return
 		//	}
 		//}
 	}
 	fmt.Println("OOps! No pizza")
 }
 
-func eatMeat(meat int) int  {
+func eatMeat(meat int) int {
 	if meat == 0 {
 		fmt.Println("Someone ate all the meat")
 		return meat
 	}
 	fmt.Println("Eating meat...")
-	return eatMeat( meat-1)
+	return eatMeat(meat - 1)
 
 }
 
 func giveForks(diners []int) []int {
 
 	var workForks []int
-	var first ,second,third int
+	var first, second, third int
 
 	for i := range diners {
 		//var max ,maxIndex int
 
-			if diners[i] > first{
-				third = second
-				second = first
-				first = diners[i]
-			} else if diners[i] > second {
-				third = second
-				second = diners[i]
-			} else if diners[i] > third{
-				third = diners[i]
+		if diners[i] > first {
+			third = second
+			second = first
+			first = diners[i]
+		} else if diners[i] > second {
+			third = second
+			second = diners[i]
+		} else if diners[i] > third {
+			third = diners[i]
 		}
 
 	}
-	workForks = append(workForks,first,second,third)
+	workForks = append(workForks, first, second, third)
 	fmt.Println("Get Forks")
 	fmt.Println(workForks)
 	return workForks
 }
-
 
 func main() {
 	cupe := []int{12}
@@ -78,7 +77,7 @@ func main() {
 	pizzaDelivery(0)
 	eatMeat(3)
 
-	diners := []int{2,88,87,42,16,10,34,1,43,100}
+	diners := []int{2, 88, 87, 42, 16, 10, 34, 1, 43, 100}
 	giveForks(diners)
 
 }
